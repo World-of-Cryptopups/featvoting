b4f84978a4ba9bd201ce1a71a31914a4c42b43b2
TheBoringDude <iamcoderx@gmail.com>
2021-10-11 11:05:21 +0800

    implement functions

--- include/featvoting.hpp
+++ include/featvoting.hpp
@@ -1,5 +1,6 @@
 #include <eosio/eosio.hpp>
 #include <eosio/singleton.hpp>
+#include <eosio/system.hpp>
 
 using namespace std;
 using namespace eosio;
@@ -22,12 +23,12 @@ public:
   // user actions
   ACTION ureguser(name user);
   ACTION usubmitfeat(name user, string title);
-  ACTION uvote(name voter, name author);
+  ACTION uvote(name voter, name author, uint64_t pollid);
 
   // admin actions
-  ACTION xcpoll(string title, string description, uint64_t startdate, uint64_t enddate);
-  ACTION xpolladdfeat(name author);
-  ACTION xpollremfeat(name author);
+  ACTION xcpoll(string title, string description, int64_t startdate, int64_t enddate);
+  ACTION xpolladdfeat(name author, uint64_t pollid);
+  ACTION xpollremfeat(name author, uint64_t pollid);
   ACTION xapprfeat(name author);
   ACTION xdapprfeat(name author);
   ACTION xeraseafeats();
@@ -57,36 +58,42 @@ private:
     uint64_t primary_key() const { return author.value; };
   };
 
-  TABLE events_s
+  TABLE polls_s
   {
     uint64_t id;
     string title;
     string description;
     vector<Feat> feats;
-    uint64_t startdate;
-    uint64_t enddate;
+    int64_t startdate;
+    int64_t enddate;
 
     uint64_t primary_key() const { return id; };
   };
 
   TABLE cvotes_s
   {
+    uint64_t voteid;
     name user;
     name feat_author;
+    uint64_t pollid;
 
-    uint64_t primary_key() const { return user.value; };
+    uint64_t primary_key() const { return voteid; };
+    uint64_t by_user() const { return user.value; };
+    uint64_t by_pollid() const { return pollid; };
   };
 
   TABLE config_s
   {
-    uint64_t eventscounter;
+    uint64_t pollscounter = 100000;
+    uint64_t votescounter = 100000;
   };
 
   // types
   typedef multi_index<"users"_n, users_s> users_t;
   typedef multi_index<"subfeats"_n, subfeats_s> subfeats_t;
   typedef multi_index<"apprfeats"_n, apprfeats_s> apprfeats_t;
-  typedef multi_index<"currentvotes"_n, cvotes_s> cvotes_t;
+  typedef multi_index<"currentvotes"_n, cvotes_s, indexed_by<"user"_n, const_mem_fun<cvotes_s, uint64_t, &cvotes_s::by_user>>, indexed_by<"pollid"_n, const_mem_fun<cvotes_s, uint64_t, &cvotes_s::by_pollid>>> cvotes_t;
+  typedef multi_index<"polls"_n, polls_s> polls_t;
   typedef singleton<"config"_n, config_s> config_t;
 
   // tables
@@ -95,4 +102,42 @@ private:
   subfeats_t subfeats = subfeats_t(get_self(), get_self().value);
   apprfeats_t apprfeats = apprfeats_t(get_self(), get_self().value);
   cvotes_t cvotes = cvotes_t(get_self(), get_self().value);
+  polls_t polls = polls_t(get_self(), get_self().value);
+
+  // checks if user has voted already to pollid
+  bool has_voted(name user, uint64_t pollid)
+  {
+    auto idx = cvotes.get_index<"user"_n>();
+
+    for (auto itr = idx.begin(); itr != idx.end(); itr++)
+    {
+      if (itr->user == user && itr->pollid == pollid)
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // checks if poll exists in
+  bool author_exists(vector<Feat> feats, name author)
+  {
+    for (Feat i : feats)
+    {
+      if (i.author == author)
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // get current ts
+  int64_t now()
+  {
+    return current_time_point().time_since_epoch().count();
+    // return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+  }
 };
--- src/featvoting.cpp
+++ src/featvoting.cpp
@@ -45,20 +45,117 @@ ACTION featvoting::usubmitfeat(name author, string title)
 }
 
 // vote a feature request
-ACTION featvoting::uvote(name user, name author)
+ACTION featvoting::uvote(name user, name author, uint64_t pollid)
 {
   require_auth(user);
 
-  auto _vfeats_it = apprfeats.find(author.value);
+  auto p_itr = polls.find(pollid);
 
-  check(_vfeats_it != apprfeats.end(), "Author's feature does not exist!");
-  check(cvotes.find(user.value) == cvotes.end(), "You have already voted a feature request!");
+  // check if event does not exist
+  check(p_itr != polls.end(), "Poll does not exist!");
 
-  cvotes.emplace(get_self(), [&](auto &row)
+  // check if author exists in poll
+  check(author_exists(p_itr->feats, author), "Author does not exist in poll!");
+
+  // check if user has voted to poll
+  check(has_voted(user, pollid), "You have already voted!");
+
+  // get voteid counter
+  config_s current_config = config.get();
+  uint64_t voteid = current_config.votescounter++;
+  config.set(current_config, get_self());
+
+  // add vote
+  cvotes.emplace(user, [&](auto &row)
                  {
+                   row.voteid = voteid;
                    row.user = user;
-                   row.feat_author = _vfeats_it->author;
+                   row.feat_author = author;
+                   row.pollid = pollid;
                  });
+}
+
+// create a poll
+ACTION featvoting::xcpoll(string title, string description, int64_t startdate, int64_t enddate)
+{
+  require_auth(get_self());
+
+  // check dates
+  check(now() < startdate, "Start Date should now be before now!");
+  check(now() < enddate && startdate < enddate, "End Date should be after now and start date!");
+
+  config_s current_config = config.get();
+  uint64_t pollid = current_config.pollscounter++;
+  config.set(current_config, get_self());
+
+  // create the poll
+  polls.emplace(get_self(), [&](auto &row)
+                {
+                  row.id = pollid;
+                  row.title = title;
+                  row.description = description;
+                  row.feats = vector<Feat>{};
+                  row.startdate = startdate;
+                  row.enddate = enddate;
+                });
+}
+
+// add feat to poll
+ACTION featvoting::xpolladdfeat(name author, uint64_t pollid)
+{
+  require_auth(get_self());
+
+  auto itr = polls.find(pollid);
+  auto f = subfeats.find(author.value);
+
+  // check if feat exists
+  check(f != subfeats.end(), "Feature request of author does not exist!");
+
+  // check if poll exists
+  check(itr != polls.end(), "Poll does not exist!");
+
+  // check if author exists in poll
+  check(!author_exists(itr->feats, author), "Author already exists in poll!");
+
+  vector<Feat> fx = itr->feats;
+  fx.push_back(Feat{f->author, f->title});
+
+  // remove author's feat from submitted ones
+  subfeats.erase(f);
+
+  // add feat to feats
+  polls.modify(itr, get_self(), [&](auto &row)
+               { row.feats = fx; });
+}
+
+// remove feat from poll
+ACTION featvoting::xpollremfeat(name author, uint64_t pollid)
+{
+  require_auth(get_self());
+
+  auto itr = polls.find(pollid);
+
+  // check if poll exists
+  check(itr != polls.end(), "Poll does not exist!");
+
+  // check if author exists in poll
+  check(author_exists(itr->feats, author), "Author does not exist in poll!");
+
+  vector<Feat> fx = itr->feats;
+  auto item = find_if(fx.begin(), fx.end(), [&](const Feat &e)
+                      { return e.author == author; }); // get_feat(fx, author);
+
+  // remove from poll
+  fx.erase(item);
+  polls.modify(itr, get_self(), [&](auto &row)
+               { row.feats = fx; });
+
+  // re-add to subfeats
+  subfeats.emplace(get_self(), [&](auto &row)
+                   {
+                     row.author = item->author;
+                     row.title = item->title;
+                   });
 }
 
 // approve a feature request
@@ -104,7 +201,7 @@ ACTION featvoting::xdapprfeat(name author)
 }
 
 // remove all approved feats
-ACTION featvoting::xerasevfeats()
+ACTION featvoting::xeraseafeats()
 {
   require_auth(get_self());
 
@@ -116,8 +213,8 @@ ACTION featvoting::xerasevfeats()
   }
 }
 
-// remove all submittedfeats
-ACTION featvoting::xerasefeats()
+// remove all submitted feats
+ACTION featvoting::xerasesfeats()
 {
   require_auth(get_self());
 
@@ -129,4 +226,4 @@ ACTION featvoting::xerasefeats()
   }
 }
 
-EOSIO_DISPATCH(featvoting, (init)(usubmitfeat)(uvote)(xapprfeat)(xdapprfeat)(xerasevfeats)(xerasefeats))
+EOSIO_DISPATCH(featvoting, (init)(usubmitfeat)(uvote)(xcpoll)(xpolladdfeat)(xpollremfeat)(xapprfeat)(xdapprfeat)(xeraseafeats)(xerasesfeats))
